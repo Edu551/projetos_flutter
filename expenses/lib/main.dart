aaa333859cd46e54a0d109c54f6d3bd911fffc60
Eduardo Moraes Goncalves <eduardogoncalves18@gmail.com>
2022-12-20 12:35:00 -0300

    Formatação dos textos da lista de transações e intalação pacote INTL para formatação de data.

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'models/transaction.dart';
+import 'package:intl/intl.dart';
 
 void main(List<String> args) {
   runApp(const ExpensesApp());
@@ -63,22 +64,38 @@ class MyHomePage extends StatelessWidget {
                       ),
                       decoration: BoxDecoration(
                         border: Border.all(
-                          color: Colors.black,
+                          color: Colors.purple,
                           width: 2,
                         ),
                       ),
                       padding: EdgeInsets.all(10),
                       child: Text(
-                        tr.value.toString(),
+                        'R\$ ${tr.value.toStringAsFixed(2)}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple),
                       ),
                     ),
-                    Column(
-                      children: <Widget>[
-                        Text(tr.title),
-                        Text(
-                          tr.date.toString(),
-                        ),
-                      ],
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            tr.title,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Text(
+                            DateFormat('dd MMM y').format(tr.date),
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
